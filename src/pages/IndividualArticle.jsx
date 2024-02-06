8373468f28cf726dffbe3d8661562c6b90f497a9
Jack Kent <jack.kent498@gmail.com>
2024-02-06 12:13:08 +0000

    adds comment list under article

--- src/pages/IndividualArticle.jsx
+++ src/pages/IndividualArticle.jsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import axios from "axios"
+import CommentTile from "../components/CommentTile";
 
 export default function IndividualArticle () {
     const {article_id} = useParams()
+
 
     const newsApi = axios.create({
         baseURL: 'https://nc-news-p6kr.onrender.com/api/articles'
     })
 
     const [articleData, setArticleData] = useState({})
-
+    const [comments, setComments] = useState([])
     useEffect(() => {
         const getIndividualArticle = async () => {
             try {
@@ -20,16 +22,37 @@ export default function IndividualArticle () {
                 console.log(err)
             }
         }
+
+        const getComments = async () => {
+            try {
+                const response = await newsApi.get(`/${article_id}/comments`)
+                setComments(response.data.comments.reverse())
+            }catch(err){
+                console.log(err)
+            }
+        }
         getIndividualArticle()
+        getComments()
     },[])
+
+
 
     return(
         <>
         {articleData.title === undefined ? <p className="p-2">loading...</p> : <></>}
         <h1 className="text-xl py-2 text-center">{articleData.title}</h1>
         <h2 className="text-center">{articleData.author}</h2>
-        <section className="px-2">votes: {articleData.votes}</section>
+        <div className="flex justify-center">
+        <img className="" src={articleData.article_img_url}></img>
+        </div>
+        <section className="px-8">votes: {articleData.votes}</section>
         <section className="indent-4 py-4 px-2">{articleData.body}</section>
+        <section className="indent-4 p-2">Comments: </section>
+        <ul>
+            {comments.map((comment) => {
+                return <CommentTile comment={comment} key={comment.comment_id} className="m-2 p-2 grid grid-cols-2"/>
+            })}
+        </ul>
         </>
     )
 }
