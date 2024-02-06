dbb5c8235af1148a2daaa65633e9b85321ac312d
Jack Kent <jack.kent498@gmail.com>
2024-02-06 15:17:40 +0000

    adds voting to articles

--- src/pages/IndividualArticle.jsx
+++ src/pages/IndividualArticle.jsx
@@ -6,10 +6,32 @@ import CommentTile from "../components/CommentTile";
 export default function IndividualArticle () {
     const {article_id} = useParams()
 
+    const [voteError, setVoteError] = useState('')
 
     const newsApi = axios.create({
         baseURL: 'https://nc-news-p6kr.onrender.com/api/articles'
     })
+
+    const changeVote = (amount) => {
+        const newvotes = {...articleData}
+        newvotes.votes += amount
+        setVoteError('')
+        const updateVotes = async (amount) => {
+            try {
+                const response = await newsApi.patch(`/${article_id}`, {inc_votes: amount})
+
+            } catch(err){
+                console.log(err)
+                setVoteError('something went wrong, please try again')
+                console.log(amount)
+                newvotes.votes = newvotes.votes - amount
+                setArticleData(newvotes)
+            }
+        }
+        setArticleData(newvotes)
+        updateVotes(amount)
+        
+    }
 
     const [articleData, setArticleData] = useState({})
     const [comments, setComments] = useState([])
@@ -35,8 +57,6 @@ export default function IndividualArticle () {
         getComments()
     },[])
 
-
-
     return(
         <>
         {articleData.title === undefined ? <p className="p-2">loading...</p> : <></>}
@@ -45,7 +65,13 @@ export default function IndividualArticle () {
         <div className="flex justify-center">
         <img className="" src={articleData.article_img_url}></img>
         </div>
-        <section className="px-8">votes: {articleData.votes}</section>
+
+        <div className="py-2 flex flex-row space-x-2">
+            <section className="px-8">votes: {articleData.votes}</section>
+            <button className="border-2 p-1 rounded-md border-sky-500" onClick={() => changeVote(1)}>/\</button>
+            <button className="border-2 p-1 rounded-md border-amber-500" onClick={() => changeVote(-1)}>\/</button>
+            {voteError ? <p className="text-red-600">{voteError}</p> : null}
+        </div>
         <section className="indent-4 py-4 px-2">{articleData.body}</section>
         <section className="indent-4 p-2">Comments: </section>
         <ul>
